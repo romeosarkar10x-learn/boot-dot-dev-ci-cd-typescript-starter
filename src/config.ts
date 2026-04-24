1c87e9bdc4bd3684310d399181102b3c97e83838
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-04-24 23:23:07 +0530

    Formatted using prettier

--- src/config.ts
+++ src/config.ts
@@ -2,25 +2,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 type Config = {
-  db: DBConfig;
-  api: APIConfig;
+    db: DBConfig;
+    api: APIConfig;
 };
 
 type APIConfig = {
-  port: string | undefined;
-  filepathRoot: string;
+    port: string | undefined;
+    filepathRoot: string;
 };
 
 type DBConfig = {
-  url: string | undefined;
+    url: string | undefined;
 };
 
 export const config: Config = {
-  api: {
-    port: process.env.PORT,
-    filepathRoot: "./src/assets",
-  },
-  db: {
-    url: process.env.DATABASE_URL,
-  },
+    api: {
+        port: process.env.PORT,
+        filepathRoot: "./src/assets",
+    },
+    db: {
+        url: process.env.DATABASE_URL,
+    },
 };
