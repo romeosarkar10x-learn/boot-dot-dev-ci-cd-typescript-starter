0da70d1380fd916847519246a1bae66bfe0f6f5e
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-04-24 18:13:29 +0530

    Removed old tests file; Added proper tests for 100% code coverage of api/auth

---
+++ tests/api/auth.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../../src/api/auth.ts";
+import { IncomingHttpHeaders } from "http";
+
+describe("Get api key", () => {
+    test("valid api key", () => {
+        const headers: IncomingHttpHeaders = {
+            "authorization": "ApiKey api_key_123"
+        };
+
+        expect(getAPIKey(headers)).toBe("api_key_123");
+    })
+
+    test("invalid authorization header", () => {
+        const headers: IncomingHttpHeaders = {
+            "authorization": "Bearer bearer_token_123"
+        };
+
+        expect(getAPIKey(headers)).toBe(null);
+    });
+
+    test("invalid authorization header 2", () => {
+        const headers: IncomingHttpHeaders = {
+            "authorization": "authorization"
+        };
+
+        expect(getAPIKey(headers)).toBe(null);
+    });
+    
+    test("authorization header not present", () => {
+        const headers: IncomingHttpHeaders = {
+            a: "1",
+            b: "2",
+            c: "3"
+        };
+
+        expect(getAPIKey(headers)).toBe(null);
+    });
+});
